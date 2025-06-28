d575ff6009ef6f8c875289cbbbb8bc7002d4beea
Aleksei Brevnov <brevnov.aleksei@mail.ru>
2025-06-28 19:40:54 +0300

    hw_39: Включаемые компоненты и контекстный процессор

---
+++ config/context_processors.py
@@ -0,0 +1,9 @@
+def site_settings(request):
+    return {
+        "site_name": 'Барбершоп "Стальная Борода"',
+        "site_description": "Профессиональные услуги барберов",
+        "main_phone": "+7 (123) 456-78-90",
+        "main_email": "info@steelbeard.ru",
+        "working_hours": "Ежедневно с 10:00 до 22:00",
+        "address": "г. Москва, ул. Барберов, д. 13",
+    }
